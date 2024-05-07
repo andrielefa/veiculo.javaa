1ecc0a0bbee3921570e9b911a11da721b4ef22da
andrielefa <144621938+andrielefa@users.noreply.github.com>
2024-05-06 22:46:26 -0300

    veiculo.java

---
+++ veiculo.java
@@ -0,0 +1,50 @@
+public abstract class Veiculo {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int anoModelo;
+    private int anoFabricacao;
+    private String chassi;
+    private String renavam;
+    private String procedencia;
+    private double capacidadeCarga;
+    private double altura;
+    private double largura;
+    private double profundidade;
+    private String corExterna;
+    private String corInterna;
+    private String tipoCombustivel;
+    private String motor;
+    private double quilometragem;
+    private double consumoMedio;
+    private int numeroPortas;
+    private int numeroPassageiros;
+    private String[] opcionais;
+
+  
+    public Veiculo() {
+
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoModelo = anoModelo;
+        this.anoFabricacao = anoFabricacao;
+        this.chassi = chassi;
+        this.renavam = renavam;
+        this.procedencia = procedencia;
+        this.capacidadeCarga = capacidadeCarga;
+        this.altura = altura;
+        this.largura = largura;
+        this.profundidade = profundidade;
+        this.corExterna = corExterna;
+        this.corInterna = corInterna;
+        this.tipoCombustivel = tipoCombustivel;
+        this.motor = motor;
+        this.quilometragem = quilometragem;
+        this.consumoMedio = consumoMedio;
+        this.numeroPortas = numeroPortas;
+        this.numeroPassageiros = numeroPassageiros;
+        this.opcionais = opcionais;
+    }
+
+}
